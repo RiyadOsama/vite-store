0bd3c46b7e26a48b0be6d0082a56c4634998915e
RiyadOsama <riyadosama14112000@gmail.com>
2025-10-03 00:32:04 +0300

    🐛 fix: Fixing Speed Insights

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { injectSpeedInsights } from "@vercel/speed-insights";
 import App from "./App.jsx";
+
+injectSpeedInsights();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <SpeedInsights />
   </StrictMode>
 );
